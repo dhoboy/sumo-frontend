572491863779e5ac4b63a2fe73c47aeb541111e1
dhoboy <dhoverstreet@gmail.com>
2022-10-02 01:52:49 +0900

    Improving Loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,15 @@
-import React, { useState, useRef, useLayoutEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import rikishiSvg_dark from "../assets/rikishi-dark.svg";
 import styles from "./styles/Loader.module.css";
 
-const Loader = ({ loading, error, errorMsg, children }) => {
+// size is "small", "medium", or "large"
+const Loader = ({ loading, error, errorMsg, size, children, className }) => {
   const [display, setDisplay] = useState(""); // loader, component, error
-  const startTime = useRef(new Date());
+  const startTime = useRef(null);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
+    startTime.current = new Date();
+
     if (loading) setDisplay("loading");
     if (error) setDisplay("error");
 
@@ -16,13 +19,24 @@ const Loader = ({ loading, error, errorMsg, children }) => {
       if (endTime - startTime.current >= 4000) {
         setDisplay("component");
       } else {
-        setTimeout(() => setDisplay("component"), 2000);
+        setTimeout(() => {
+          setDisplay("component");
+        }, 2000);
       }
     }
+    return () => (startTime.current = null);
   }, [error, loading, setDisplay]);
 
   if (display === "loading") {
-    return <img src={rikishiSvg_dark} alt="loading" className={styles.img} />;
+    return (
+      <div className={`${styles.wrapper} ${className}`}>
+        <img
+          src={rikishiSvg_dark}
+          alt="loading"
+          className={`${styles.img} ${styles[size]}`}
+        />
+      </div>
+    );
   } else if (display === "error") {
     return <div>{errorMsg || "An error occured"}</div>;
   }
